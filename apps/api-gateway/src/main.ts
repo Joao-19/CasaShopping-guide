f87ed7a2a6d892d6caf86f9328d3bed53ede5f7e
joao <jjjoaovitor@gmail.com>
2025-12-17 23:19:44 -0300

    hot-fix: resolving cors

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
   console.log(`🚀 Auth Service (NestJS) rodando na porta ${port}`);
 }
 bootstrap();
