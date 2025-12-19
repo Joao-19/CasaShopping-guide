2b412eebd5622866339cc9878617ec58f7ec35cb
joao <jjjoaovitor@gmail.com>
2025-12-18 23:30:16 -0300

    fix dockerfiles alpine / configuration

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   app.enableCors();
   const port = process.env.PORT || 3003;
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
   console.log(`🚀 Auth Service (NestJS) rodando na porta ${port}`);
 }
 bootstrap();
--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   app.enableCors();
 
   const port = process.env.PORT || 3004;
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
 
   console.log(`🚀 Auth Service (NestJS) rodando na porta ${port}`);
 }
