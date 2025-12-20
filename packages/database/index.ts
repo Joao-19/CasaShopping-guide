ee295f9801a1b38a3c7d8d0a321b22383a6eb2f2
joao <jjjoaovitor@gmail.com>
2025-12-20 00:49:56 -0300

    feat: Adicionando esteira CI/CD

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,8 +1,8 @@
 // packages/database/index.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/client";
 
 // Cria a instância da conexão com o banco
 export const prisma = new PrismaClient();
 
 // Exporta também os tipos (User, Admin, etc) para usar no código
-export * from "@prisma/client";
+export * from "./generated/client";
