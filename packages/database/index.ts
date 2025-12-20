573f02870daf7cd88ff0051609ea4ea497d09d05
joao <jjjoaovitor@gmail.com>
2025-12-20 10:02:58 -0300

    feat: Fix prisma client imports

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,8 +1,8 @@
 // packages/database/index.ts
-import { PrismaClient } from "./generated/client";
+import { PrismaClient } from "./generated/client/index.js";
 
 // Cria a instância da conexão com o banco
 export const prisma = new PrismaClient();
 
 // Exporta também os tipos (User, Admin, etc) para usar no código
-export * from "./generated/client";
+export * from "./generated/client/index.js";
