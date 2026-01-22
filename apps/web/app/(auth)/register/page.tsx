81a4dc9253f9812ad070b876f9985da7be3ba4ea
joao <jjjoaovitor@gmail.com>
2026-01-22 13:45:33 -0300

    feat: add fix turbo.json for base path variable and remove .env.example for gitgnore

--- apps/web/app/(auth)/register/page.tsx
+++ apps/web/app/(auth)/register/page.tsx
@@ -155,7 +155,7 @@ const RegisterPage = () => {
             {/* Background Image */}
             <div className="fixed inset-0 z-0">
                 <img
-                    src="/login-bg.webp"
+                    src={`${process.env.BASE_PATH || ""}/login-bg.webp`}
                     alt="Interior Background"
                     className="w-full h-full object-cover"
                 />
