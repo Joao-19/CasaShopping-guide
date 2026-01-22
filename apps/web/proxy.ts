c3f68c27e2753e1ac8e5d5fefc3c75e108c407d9
joao <jjjoaovitor@gmail.com>
2026-01-22 15:06:13 -0300

    feat: add fix turbo.json for base path variable and remove .env.example for gitgnore

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const basePath = process.env.BASE_PATH || "/casashopping";
+
 // 1. Defina APENAS as rotas que NÃO precisam de auth
 const publicRoutes = ["/login", "/register", "/", "/stores", "/produtos"];
 
@@ -8,27 +10,28 @@ export function proxy(request: NextRequest) {
   // Tenta pegar o token
   const token = request.cookies.get("access_token")?.value;
 
-  // Pega a rota atual
+  // Pega a rota atual (remove o basePath para comparação)
   const { pathname } = request.nextUrl;
+  const pathWithoutBase = pathname.replace(basePath, "") || "/";
 
   // Verifica se a rota atual está na lista de públicas
-  const isPublicRoute = publicRoutes.includes(pathname);
+  const isPublicRoute = publicRoutes.includes(pathWithoutBase);
 
   // --- CENÁRIO 1: Usuário DESLOGADO tentando acessar rota PRIVADA ---
   // Se não é pública (ex: / ou /dashboard) e não tem token
   if (!isPublicRoute && !token) {
-    // Redireciona para o login e salva a URL que ele queria acessar (opcional, boa prática)
-    const loginUrl = new URL("/login", request.url);
+    // Redireciona para o login
+    const loginUrl = new URL(`${basePath}/login`, request.url);
     return NextResponse.redirect(loginUrl);
   }
 
   // --- CENÁRIO 2: Usuário LOGADO tentando acessar rota de LOGIN ---
   // Se ele já tem token e tenta entrar no login ou register
-  const isAuthRoute = ["/login", "/register"].includes(pathname);
+  const isAuthRoute = ["/login", "/register"].includes(pathWithoutBase);
 
   if (isAuthRoute && token) {
     // Redireciona para o Dashboard (Home)
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL(`${basePath}/`, request.url));
   }
 
   // Se passou por tudo, libera o acesso
