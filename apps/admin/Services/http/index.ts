640f100f470fc0f80ff2b1250c5efcf0b68ad462
joao <jjjoaovitor@gmail.com>
2026-01-24 11:15:17 -0300

    feat: Fix remove admin base path

--- apps/admin/Services/http/index.ts
+++ apps/admin/Services/http/index.ts
@@ -1,6 +1,6 @@
 import { createApiClient } from "@repo/api-client";
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "/admin";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const getBaseUrl = () => {
   const url = process.env.NEXT_PUBLIC_API_URL;
